5f00304d93f5db9d525fb7747872c6b85ac883c8
Amrik Sadhra <AmrikSadhra@gmail.com>
2018-10-18 10:42:54 +0100

    Some changes to g3log cmake script to work better under CI

--- cmake/g3log.cmake
+++ cmake/g3log.cmake
@@ -1,34 +1,39 @@
 set(G3LOG_NAME g3log)
 
+set(G3LOGGER_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/lib/${G3LOG_NAME}/build")
+
 # Specifically pick the static version since the build generates both static and dynamic libraries
-set(G3LOGGER_LIBRARIES "${CMAKE_CURRENT_BINARY_DIR}/lib/${G3LOG_NAME}/build/libg3logger.a")
+if (MSVC)
+    set(G3LOGGER_LIBRARIES "${G3LOGGER_BUILD_DIR}/libg3logger.lib")
+else ()
+    set(G3LOGGER_LIBRARIES "${G3LOGGER_BUILD_DIR}/libg3logger.a")
+endif ()
 
-set(GENERATOR "CodeBlocks - MinGW Makefiles")
+#[[On Windows, the generator for g3logger seems to change to MSVC automatically]]
+if (MINGW)
+    set(GENERATOR "CodeBlocks - MinGW Makefiles")
+elseif (MSVC)
+    set(GENERATOR "Visual Studio 15 2017")
+else ()
+    set(GENERATOR ${CMAKE_GENERATOR})
+endif ()
 
-add_definitions()
-
-if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/lib/${G3LOG_NAME}/build)
+if (NOT EXISTS G3LOGGER_BUILD_DIR)
     message("Configuring G3log")
-    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib/${G3LOG_NAME}/build)
+    file(MAKE_DIRECTORY ${G3LOGGER_BUILD_DIR})
     execute_process(COMMAND ${CMAKE_COMMAND}
             "-G${GENERATOR}"
             "-DCMAKE_BUILD_TYPE=Release"
             "-DG3_SHARED_LIB=OFF"
             "-H${DEP_ROOT_DIR}/${G3LOG_NAME}"
-            "-B${CMAKE_CURRENT_BINARY_DIR}/lib/${G3LOG_NAME}/build"
+            "-B${G3LOGGER_BUILD_DIR}"
             RESULT_VARIABLE
             G3LOG_CONFIGURE)
-    if(NOT G3LOG_CONFIGURE EQUAL 0)
+    if (NOT G3LOG_CONFIGURE EQUAL 0)
+        file(REMOVE ${G3LOGGER_BUILD_DIR})
         message(FATAL_ERROR "G3log Configure failed!")
-    endif()
-
-    #[[ TODO: Move this from project-gen time to build time, as a target ]]
-    message("Building G3log")
-    execute_process(COMMAND ${CMAKE_COMMAND} --build
-            "${CMAKE_CURRENT_BINARY_DIR}/lib/${G3LOG_NAME}/build"
-            RESULT_VARIABLE
-            G3LOG_BUILD)
-    if(NOT G3LOG_BUILD EQUAL 0)
-        message(FATAL_ERROR "G3log build failed!")
-    endif()
-endif()
+        return()
+    else ()
+        add_custom_target(g3logger COMMAND ${CMAKE_COMMAND} --build ${G3LOGGER_BUILD_DIR})
+    endif ()
+endif ()
