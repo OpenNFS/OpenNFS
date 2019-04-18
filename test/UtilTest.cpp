979b2761c842d0ab13c3c50c2b64f8528749e20c
Amrik Sadhra <AmrikSadhra@gmail.com>
2019-04-19 00:09:52 +0100

    Convert to test fixture to allow setup and teardown per test

--- test/UtilTest.cpp
+++ test/UtilTest.cpp
@@ -1,12 +1,82 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 
 #include "gtest/gtest.h"
+
+#include "../src/Util/Logger.h"
 #include "../src/Util/Utils.h"
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <map>
 #include <boost/filesystem.hpp>
 
-TEST(UtilTest, ExtractVIV){
+
+class UtilTest : public testing::Test {
+public:
+    virtual void SetUp()
+    {
+        // Initialise Logging
+        std::shared_ptr<Logger> logger = std::make_shared<Logger>();
+        // And OpenGL
+        InitOpenGL();
+    }
+
+    virtual void TearDown() {
+        glfwTerminate();
+    }
+private:
+    GLFWwindow *window;
+    static void glfwError(int id, const char *description) {
+        LOG(WARNING) << description;
+    }
+    void InitOpenGL() {
+        // Initialise GLFW
+        ASSERT(glfwInit(), "GLFW Init failed.\n");
+        glfwSetErrorCallback(&glfwError);
+        // TODO: Disable MSAA for now until texture array adds padding
+        glfwWindowHint(GLFW_SAMPLES, 4);
+
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Appease the OSX Gods
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#else
+        // TODO: If we fail to create a GL context on Windows, fall back to not requesting any (Keiiko Bug #1)
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+#endif
+        window = glfwCreateWindow(10, 10, "OpenNFS Test", nullptr, nullptr);
+        if (window == nullptr) {
+            LOG(WARNING) << "Failed to create a GLFW window.";
+            getchar();
+            glfwTerminate();
+            GTEST_FAIL();
+        }
+        glfwMakeContextCurrent(window);
+        // Initialize GLEW
+        glewExperimental = GL_TRUE; // Needed for core profile
+        if (glewInit() != GLEW_OK) {
+            LOG(WARNING) << "Failed to initialize GLEW";
+            getchar();
+            glfwTerminate();
+            GTEST_FAIL();
+        }
+        // Ensure we can capture the escape key being pressed below
+        glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+        // Set the mouse at the center of the screen
+        glfwPollEvents();
+        // Dark blue background
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        // Enable depth test
+        glEnable(GL_DEPTH_TEST);
+        // Accept fragment if it closer to the camera than the former one
+        glDepthFunc(GL_LESS);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+    }
+};
+
+TEST_F(UtilTest, ExtractVIV){
     using namespace boost::filesystem;
 
     const std::string vivPath = "../resources/NFS_3/gamedata/carmodel/diab/car.viv";
