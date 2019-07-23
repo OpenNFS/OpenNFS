22584a65f82e91f5f31fa3cdbd1e936d5be6cf9d
Amrik Sadhra <AmrikSadhra@gmail.com>
2019-07-23 19:51:59 +0100

    Yeet

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,9 +1,11 @@
 #include "Config.h"
 
+#define yeet throw
+
 void option_dependency(const variables_map &vm, const char *for_what, const char *required_option) {
     if (vm.count(for_what) && !vm[for_what].defaulted())
         if (vm.count(required_option) == 0 || vm[required_option].defaulted())
-            throw std::logic_error(std::string("Option '") + for_what + "' requires option '" + required_option + "'.");
+            yeet std::logic_error(std::string("Option '") + for_what + "' requires option '" + required_option + "'.");
 }
 
 void Config::ParseFile(std::ifstream &inStream) {
