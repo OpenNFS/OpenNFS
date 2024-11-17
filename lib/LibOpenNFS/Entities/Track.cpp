b00f5505bec781c20158deca08f6ec61e9e0fc98
Amrik Sadhra <amriksadhra@gmail.com>
2024-11-17 12:28:30 +0000

    Add LibOpenNFS as a submodule

--- lib/LibOpenNFS/Entities/Track.cpp
+++
@@ -1,6 +0,0 @@
-#include "Track.h"
-
-namespace LibOpenNFS {
-    Track::Track(NFSVersion _nfsVersion, std::string const& _name, std::string const& _basePath) : nfsVersion(_nfsVersion), name(_name), basePath(_basePath) {
-    }
-} // namespace LibOpenNFS
--- lib/LibOpenNFS/Entities/TrackSound.cpp
+++
@@ -1,8 +0,0 @@
-#include "TrackSound.h"
-
-namespace LibOpenNFS {
-    TrackSound::TrackSound(uint32_t entityID, glm::vec3 position, uint32_t type) : TrackEntity(entityID, EntityType::SOUND, 0u) {
-        this->position = position;
-        this->type     = type;
-    }
-} // namespace LibOpenNFS
