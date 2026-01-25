65859a1fd8217e2c9b3783facf9421e1e5ddcc5f
Wouter Wijsman <wwijsman@live.nl>
2026-01-25 16:10:34 +0100

    Add new CarMenuData class

---
+++ src/Menu/CarMenuData.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <Common/NFSVersion.h>
+#include <NFS3/FEDATA/FedataFile.h>
+
+namespace OpenNFS {
+    class CarMenuData {
+      public:
+        CarMenuData(NFSVersion _nfsVersion, std::string const &carId);
+
+        bool LoadMenuData(LibOpenNFS::NFS3::FedataFile &fedataFile);
+
+        NFSVersion nfsVersion;
+        std::string id;
+        std::string carName;
+        std::vector<std::string> colorNames; 
+    };
+} // namespace OpenNFS
